37a5dc97ae4ebcd4fc803c4d8bccc3b4304864bc
VeagleV <lanselot4267@gmail.com>
2026-03-05 15:15:36 +0300

    fixed null problem caused by wrong json parsing

--- core/src/main/java/com/crm/item/core/dtos/ItemListResponse.java
+++ core/src/main/java/com/crm/item/core/dtos/ItemListResponse.java
@@ -1,6 +1,7 @@
 package com.crm.item.core.dtos;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +14,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Schema(name = "DTO товара(Response)")
 public class ItemListResponse {
+    @JsonProperty("warehouse_id")
     private Integer warehouseId;
+    @JsonProperty("item_id")
     private Integer itemId;
+    @JsonProperty("quantity")
     private Integer quantity;
 }
